8406802bc0bf2d99a79ae011c0f51c738ea23eba
NuGelaLies <nugelalies@gmail.com>
2021-04-21 17:39:01 +0800

    - add 113, 117, 124, 129, 173, 199, 222

---
+++ LeetCode_Swift/117.填充每个节点的下一个右侧节点指针-ii.swift
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=swift
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var left: Node?
+ *     public var right: Node?
+ *	   public var next: Node?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func connect(_ root: Node?) -> Node? {
+        
+    }
+}
+// @lc code=end
+
---
+++ LeetCode_Swift/173.二叉搜索树迭代器.swift
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=173 lang=swift
+ *
+ * [173] 二叉搜索树迭代器
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+class BSTIterator {
+
+    init(_ root: TreeNode?) {
+
+    }
+    
+    func next() -> Int {
+
+    }
+    
+    func hasNext() -> Bool {
+
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * let obj = BSTIterator(root)
+ * let ret_1: Int = obj.next()
+ * let ret_2: Bool = obj.hasNext()
+ */
+// @lc code=end
+
